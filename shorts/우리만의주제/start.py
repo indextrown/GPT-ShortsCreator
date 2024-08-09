976cc8b9e70f0748450e9b1a178999a44bbdb348
indextrown <lghan00020@nate.com>
2024-08-09 17:04:01 +0900

    upload

--- shorts/우리만의주제/start.py
+++ shorts/우리만의주제/start.py
@@ -1,5 +1,5 @@
 # 프롬포트 텍스트 가져오기
-with open('prompt.text', 'r', encoding='utf-8') as file:
-    content = file.read()
+with open("prompt.text", encoding="utf-8") as file:
+    file = file.read()
 
-print(content)
+print(file)
